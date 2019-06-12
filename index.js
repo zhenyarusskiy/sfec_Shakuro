1199a4c16a17cd1736bfcea37d205c86f26a2f26
zhenya.russkiy <russkiy.zhenya@gmail.com>
2019-06-12 20:58:47 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,2 @@
+const style = require("./src/css/main.scss");
+const js = require("./src/js/common");
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ETP = require("extract-text-webpack-plugin");
+var imageWebpackLoader = require("image-webpack-loader");
+
+module.exports = {
+  entry: {
+    app: './index.js'
+  },
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css|sass|scss)$/,
+        loader: ETP.extract({
+          fallback: "style-loader",
+          use: "css-loader!sass-loader"
+        })
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              
+              disable: true
+            },
+          },
+        ],
+      }
+    ]
+  },
+  devServer: {
+    overlay: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Cards",
+      minify: {
+        collapseWhitespace: true
+      },
+      hash: true,
+      template: "./index.html"
+    }),
+    new ETP("main.css")
+  ]
+}
